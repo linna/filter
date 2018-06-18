0103408996a45cad3d5705601a46629ead39746f
s3b4stian <sebastian.rapetti@alice.it>
2018-06-18 14:50:40 +0000

    AbstractString class added

---
+++ src/RulesHelper/AbstractString.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Linna Filter
+ *
+ * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
+ * @copyright (c) 2018, Sebastian Rapetti
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+declare(strict_types = 1);
+
+namespace Linna\Filter\Rules;
+
+/**
+ * Abstract String
+ */
+class AbstractString
+{
+    /**
+     * Sanitize.
+     *
+     * @param mixed $value
+     */
+    public function sanitize(&$value): void
+    {
+        settype($value, 'string');
+    }
+}
